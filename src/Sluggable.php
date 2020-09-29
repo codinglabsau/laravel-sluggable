4bf9a3d5a8b8efc55649d1ecc2146be5c28de3b8
stevethomas <steve@codinglabs.io>
2020-09-29 10:48:16 +1000

    initial files

---
+++ src/Sluggable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Codinglabs\Sluggable;
+
+use Illuminate\Support\Str;
+
+trait Sluggable
+{
+    public static function findBySlug(string $slug): self
+    {
+        return self::where('slug', strtolower($slug))->first();
+    }
+
+    public function setNameAttribute(string $value): void
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
